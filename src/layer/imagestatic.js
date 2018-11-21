5705443c8ccaf66fbcfdf7ee0bf4503f54f39b9f
Per Fröjd <per.frojd@decerno.se>
2018-11-21 16:47:14 +0100

    Maybe works

--- src/layer/imagestatic.js
+++ src/layer/imagestatic.js
@@ -1,5 +1,6 @@
 import ImageStatic from 'ol/source/imagestatic';
 import GeoJSON from 'ol/format/geojson';
+import Projection from 'ol/proj/projection';
 import $ from 'jquery';
 import viewer from '../viewer';
 import image from './image';
@@ -8,7 +9,7 @@ import isUrl from '../utils/isurl';
 function createSource(options) {
   const geojsonFormat = new GeoJSON();
   const source = new ImageStatic({
-    imageExtent: [181896.33, 6101648.07, 864416, 7689478.31],
+    imageExtent: [662089.9183123151, 6560234.950785562, 689091.5072209174, 6576944.457947316],
     url: '',
     projection: viewer.getProjection(),
     imageLoadFunction: (img) => {
@@ -16,7 +17,14 @@ function createSource(options) {
         url: options.url,
         cache: false
       }).done((result) => {
-        const features = geojsonFormat.readFeatures(result);
+        const features = geojsonFormat.readFeatures(result, {
+          dataProjection: viewer.getProjection(),
+          featureProjection: viewer.getProjection()
+        });
+
+
+        // Get all of the groundOverlays from the feature result,
+        // in case they are mixed with other features.
         const groundOverlays = features.map((f) => {
           const properties = f.getProperties();
           if (properties && properties.isGroundOverlay) {
@@ -25,8 +33,12 @@ function createSource(options) {
           return null;
         }).filter(Boolean);
         groundOverlays.forEach((feature) => {
+
+          // Replace the original imageExtent with the new extent given the feature.
           source.imageExtent = feature.getGeometry().getExtent();
           const base64Image = feature.getProperties().image;
+
+          // Set the imagesrc to the base64 url.
           img.getImage().src = `data:image/png;base64,${base64Image}`;
 
           // There was no setter for this, but apparently the image has its own extent
@@ -37,7 +49,7 @@ function createSource(options) {
           // No idea if this is necessary, but it's the easiest way to get
           // the file size from the base64 image.
           const imageSize = new Image();
-          imageSize.onload = function() {
+          imageSize.onload = () => {
             source.imageSize = [imageSize.width, imageSize.height]
           }
           imageSize.src = `data:image/png;base64,${base64Image}`;
